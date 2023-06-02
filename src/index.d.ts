47f181520d28e28b87539d8a5395eee1d34921a4
Adwait Naik <adwaitnaik2@gmail.com>
2023-06-02 10:33:31 +0100

    Initial commit

---
+++ src/index.d.ts
@@ -0,0 +1,5 @@
+declare module 'aframe';
+declare module 'aframe-animation-component';
+declare module 'aframe-particle-system-component';
+declare module 'aframe-react';
+declare module 'babel-polyfill';
